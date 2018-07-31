d0ecc53303954f5095bbf33a3858f5eb6b7c6896
lizhen <lizhen@zhumengyuan.com>
2018-07-31 15:37:10 +0800

    baseline

---
+++ app/src/main/java/com/lz/sample/android_constraintlayout_sample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.lz.sample.android_constraintlayout_sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBasicAttrClick(View view) {
+        Intent intent = new Intent(MainActivity.this, BasicAttrActivity.class);
+        startActivity(intent);
+    }
+
+    public void onCompareWithRLClick(View view) {
+
+    }
+
+    public void onSetSizeAsARatio(View view) {
+
+    }
+
+    public void onChainClick(View view) {
+
+    }
+}
